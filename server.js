816cf7adf879be33430dde38abe493e881992153
fuyanik <furkan.uyanik@boun.edu.tr>
2022-10-06 18:52:02 +0300

    asd

--- server.js
+++ server.js
@@ -1,3 +1,17 @@
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+module.exports = function(app) {
+  app.use(
+    '/api',
+    createProxyMiddleware({
+      target: 'http://localhost:3000',
+      changeOrigin: true,
+    })
+  );
+};
+
+
+
 const express = require("express");
 const app = express();
 // This is your test secret API key.

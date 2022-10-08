82b19df08b651c3e39a58396846187571750f4de
fuyanik <furkan.uyanik@boun.edu.tr>
2022-10-08 23:42:22 +0300

    asd

---
+++ build/precache-manifest.fad278e7d6343d7c04f124bf52d2dbd1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0bee8be40bb17481f3ea07fa33a4e4e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4ae3e744812a782e3b65",
+    "url": "/static/css/main.9a778b51.chunk.css"
+  },
+  {
+    "revision": "b2fbc1a08869c67d9ce5",
+    "url": "/static/js/2.e550cf77.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.e550cf77.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ae3e744812a782e3b65",
+    "url": "/static/js/main.1094c062.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.1094c062.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b989597238684c4b9d92",
+    "url": "/static/js/runtime-main.97fb2e82.js"
+  }
+]);

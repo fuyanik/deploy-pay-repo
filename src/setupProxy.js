f8fa9ce8772b167c300d2c1c9e9c06028a04a356
fuyanik <furkan.uyanik@boun.edu.tr>
2022-10-12 03:25:51 +0300

    asd

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,6 +1,11 @@
-
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
-module.exports = app => {
-  app.use(createProxyMiddleware("/api/*", { target: "http://localhost:8080/" }));
+module.exports = function(app) {
+  app.use(
+    '/api',
+    createProxyMiddleware({
+      target: 'http://localhost:8080',
+      changeOrigin: true,
+    })
+  );
 };
